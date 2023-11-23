deb2561179a50a911981026f10a33ef10e9bf9b4
Alexis Calixto <alexiscfx1@gmail.com>
2023-11-23 11:37:18 -0300

    refactoring JavaScript code

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const btn1 = document.querySelector('#js__btn1')
-const btn2 = document.querySelector('#js__btn2')
-const btn3 = document.querySelector('#js__btn3')
-const btn4 = document.querySelector('#js__btn4')
-const btn5 = document.querySelector('#js__btn5')
+const btns = document.querySelectorAll('.element__btn')
 
-function active () {
-    this.nextElementSibling.classList.toggle('inactive')
-    this.firstElementChild.classList.toggle('rotate')
-    this.classList.toggle('focus')
+for(let btn of btns) {
+    btn.addEventListener('click', function () {
+        this.nextElementSibling.classList.toggle('inactive')
+        this.firstElementChild.classList.toggle('rotate')
+        this.classList.toggle('focus')
+    })
 }
-
-btn1.addEventListener('click', active)
-btn2.addEventListener('click', active)
-btn3.addEventListener('click', active)
-btn4.addEventListener('click', active)
-btn5.addEventListener('click', active)
