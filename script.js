bc63dd78c9f966a936598e5822a9e19d33495d9a
Alexis Calixto <alexiscfx1@gmail.com>
2023-11-23 12:22:11 -0300

    Using forEach to browse and listen to the disclosures

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const btns = document.querySelectorAll('.element__btn')
 
-for(let btn of btns) {
-    btn.addEventListener('click', function () {
+btns.forEach(element => {
+    element.addEventListener('click', function () {
         this.nextElementSibling.classList.toggle('inactive')
         this.firstElementChild.classList.toggle('rotate')
         this.classList.toggle('focus')
     })
-}
+});
